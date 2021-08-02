95af444b880aece4aebf2dd6ec96e5d9ea706b8e
Rahul <rkc.98@outlook.com>
2021-08-02 16:18:12 +0530

    razor pay demo

---
+++ app.py
@@ -0,0 +1,23 @@
+import razorpay
+import json
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__,static_folder = "static", static_url_path='')
+razorpay_client = razorpay.Client(auth=("<APP_ID>", "<APP_SECRET>"))
+
+
+@app.route('/')
+def app_create():
+    return render_template('app.html')
+
+
+@app.route('/charge', methods=['POST'])
+def app_charge():
+    amount = 50000
+    payment_id = request.form['razorpay_payment_id']
+    razorpay_client.payment.capture(payment_id, amount)
+    return json.dumps(razorpay_client.payment.fetch(payment_id))
+
+if __name__ == '__main__':
+    app.run()
